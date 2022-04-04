f2e31e8e6c3d9cfe3a59f968daf2838fa6f7d0fa
April <aprilnicole013@gmail.com>
2022-04-04 12:09:17 -0400

    Format date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,22 @@
 //dynamic route
 import Layout from "../../components/layout";
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
 
 export default function Post({ postData }){
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
           {postData.title}
           <br />
           {postData.id}
           <br />
-          {postData.date}
+          <Date dateString={postData.date} />
+          <br />
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
       )
 }
@@ -24,7 +31,7 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps({ params }){
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
